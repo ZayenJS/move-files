523fc2bf181cb8a6fca4684b14187596e10202e9
ZayenJS <davidnogueira.dev.pro@gmail.com>
2023-12-23 17:15:17 +0100

    add command for year/month sorted folders

--- src/Controller/FSMoveController.ts
+++ src/Controller/FSMoveController.ts
@@ -12,7 +12,7 @@ type MoveAttributes = {
   options: MoveOptions;
 };
 
-class FSMoveController extends FSController implements FSControllerType<MoveOptions>, MoveAttributes {
+export class FSMoveController extends FSController implements FSControllerType<MoveOptions>, MoveAttributes {
   public source: string = '';
   public destination: string = '';
   public options: MoveOptions = { dry: false, type: '*' };
@@ -32,6 +32,121 @@ class FSMoveController extends FSController implements FSControllerType<MoveOpti
 
     exit(0);
   };
+
+  // TODO: refactor this to be more readable
+  public runYear = async (source: string, destination: string, options: MoveOptions) => {
+    this.source = source;
+    this.destination = destination;
+    this.options = options;
+
+    const isDir = await this.isDirectory(this.source);
+
+    if (!isDir) {
+      console.info(`${this.source} is not a directory`);
+      exit(0);
+    }
+
+    const result = await this.getFilePaths(this.source);
+
+    console.info('='.repeat(80));
+    console.info(`Here are the files that ${options.dry ? 'would' : 'will'} be moved:`);
+
+    for (const [source, destination] of Object.entries(result)) {
+      console.info(`${source} => ${destination}`);
+    }
+    console.info('='.repeat(80));
+
+    if (options.dry) {
+      console.info('This is a dry run, nothing will be changed in the filesystem');
+      exit(0);
+    }
+
+    const wantToProceed = await this.rl.question('Do you want to proceed? (y/n) ');
+
+    if (wantToProceed !== 'y') {
+      console.info('Aborting');
+      exit(0);
+    }
+
+    for (const [source, destination] of Object.entries(result)) {
+      const destinationFolder = destination.replace(/\/[^/]+$/, '');
+      this.handleDestinationDoesNotExist(destinationFolder);
+
+      const destinationFileExists = Boolean(await this.fs.stat(destination).catch(() => false));
+
+      if (destinationFileExists) {
+        const answer = await this.rl.question(`File ${destination} already exists, overwrite? (y/n) `);
+
+        if (answer !== 'y') {
+          console.info(`Skipping ${destination}`);
+          continue;
+        }
+      }
+
+      try {
+        await this.fs.rename(source, destination);
+        console.info(`Moved ${source} to ${destination}`);
+      } catch (e) {
+        console.info(`Could not move ${source} to ${destination}`);
+        console.error(e);
+        exit(1);
+      }
+    }
+
+    console.info('Done');
+    exit(0);
+  };
+
+  private async isDirectory(path: string) {
+    return Boolean(
+      await this.fs
+        .stat(path)
+        .then((el) => el.isDirectory())
+        .catch(() => false),
+    );
+  }
+
+  private async getFilePaths(path: string = '', result: Record<string, string> = {}): Promise<Record<string, string>> {
+    console.info(`Getting files for ${path}`);
+
+    const sourceDirContent = await this.fs.readdir(path ?? this.source);
+    const files = sourceDirContent.filter((file) => file.endsWith(this.options.type));
+
+    if (!files.length) {
+      for (const dir of sourceDirContent) {
+        const isDir = await this.isDirectory(`${path}/${dir}`);
+
+        if (!isDir) {
+          continue;
+        }
+
+        const f = await this.getFilePaths(`${path}/${dir}`);
+
+        for (const [source, destination] of Object.entries(f)) {
+          result[source] = destination;
+        }
+      }
+    }
+
+    const year = /\/(\d{4})\//.exec(path)?.[1];
+    const month = /\/(\d{2})$/.exec(path)?.[1];
+
+    for (const file of files) {
+      let destinationPath = this.destination;
+
+      if (year) {
+        destinationPath += `/${year}`;
+      }
+
+      if (month) {
+        destinationPath += `/${month}`;
+      }
+
+      result[`${path}/${file}`] = `${destinationPath}/${file}`;
+    }
+
+    return result;
+  }
 
   private async getFilesForType(type: string) {
     const files = await this.fs.readdir(this.source);
@@ -80,13 +195,14 @@ class FSMoveController extends FSController implements FSControllerType<MoveOpti
     exit(0);
   }
 
-  private async handleDestinationDoesNotExist() {
-    const destinationExists = Boolean(await this.fs.stat(this.destination).catch(() => false));
+  private async handleDestinationDoesNotExist(destination: string = '') {
+    const destinationExists = Boolean(await this.fs.stat(destination ?? this.destination).catch(() => false));
 
     if (destinationExists) {
       return;
     }
 
+    console.info();
     const answer = await this.rl.question(`Destination folder ${this.destination} does not exist, create it? (y/n) `);
 
     if (answer !== 'y') {
@@ -94,7 +210,13 @@ class FSMoveController extends FSController implements FSControllerType<MoveOpti
       exit(0);
     }
 
-    await this.fs.mkdir(this.destination);
+    try {
+      await this.fs.mkdir(this.destination, { recursive: true });
+    } catch (e) {
+      console.info(`Could not create ${this.destination}`);
+      console.error(e);
+      exit(1);
+    }
   }
 
   private async moveFiles(files: string[]) {
@@ -112,7 +234,14 @@ class FSMoveController extends FSController implements FSControllerType<MoveOpti
         }
       }
 
-      await this.fs.rename(`${this.source}/${file}`, `${this.destination}/${file}`);
+      try {
+        await this.fs.rename(`${this.source}/${file}`, `${this.destination}/${file}`);
+        console.info(`Moved ${file} to ${this.destination}`);
+      } catch (e) {
+        console.info(`Could not move ${file} to ${this.destination}`);
+        console.error(e);
+        exit(1);
+      }
     }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,22 @@ import { fsMoveController } from './Controller/FSMoveController';
 
 const program = new Command();
 
+program.version('1.0.0');
+
 program
-  .version('1.0.0')
   .command('mv <source> <destination>')
   .requiredOption('-t, --type <type>', 'Type of file to move (e.g. .jpg, .png, .txt, etc.)')
   .option('-d, --dry', 'Dry run, do not move files but show what would be moved')
   .description('Move files from one directory to another')
   .action(fsMoveController.run);
+
+program
+  .command('mv-year <source> <destination>')
+  .requiredOption('-t, --type <type>', 'Type of file to move (e.g. .jpg, .png, .txt, etc.)')
+  .option('-d, --dry', 'Dry run, do not move files but show what would be moved')
+  .description(
+    'Source directory should contain year directories, which should contain month directories, which should contain files (e.g. 2021/01/1234.mp4)',
+  )
+  .action(fsMoveController.runYear);
 
 program.parse(process.argv);
